d8357ab195b33cb7cc032f1356b9a661c84c85fa
Aditya Mitra <adityamitra1911@gmail.com>
2021-02-08 04:02:15 +0530

    Added individual responses, post route responses and list of participants.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ var passport = require('passport')
 require("./passport/passportgoogle")(passport);
 require("./passport/passportjwt")(passport);
 var jwt = require("jsonwebtoken");
+const cookieSession = require('cookie-session');
 
 const MONGO_URL = "mongodb://localhost/auditions";
 
@@ -19,11 +20,17 @@ mongoose.connect(MONGO_URL, {
 });
 app.use(cors());
 app.use(express.json());
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //token expires after 30 days
+    keys: ["aditya"],
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize())
 app.use(passport.session());
 
 app.get("/", (req, res) => {
+  console.log("Hello", req.user)
   res.send("Hello World");
 });
 
@@ -45,16 +52,26 @@ app.get(
   "/auth/google/redirect",
   passport.authenticate("google"),
   (req, res) => {
-    const payload = {
-      id: req.user._id,
-      name: req.user.name,
-      email: req.user.email
-    };
-    var token = jwt.sign(payload, process.env.SECRET, { expiresIn: 600000 });
-
-    res.redirect(`${process.env.FRONTEND}?token=${token}`);
+    console.log(req.user)
+    res.json(req.user)
   }
 );
+
+// CHECK AUTHENTICATED //
+
+app.get("/api/current", checkauthentication, (req, res) => {
+  console.log(req.user)
+  res.send(true)
+})
+
+function checkauthentication(req, res, next){
+  if(req.isAuthenticated()){
+      //req.isAuthenticated() will return true if user is logged in
+      next();
+  } else{
+    res.send(false)
+  }
+}
 
 // QUESTIONS //
 
@@ -115,7 +132,7 @@ app.put("/api/questionsupdate", async (req, res) => {
         await questions.findByIdAndUpdate(req.body.qid, { content: req.body.content }, (err, docs) => {
           if(err){
             console.log(err);
-            console.log("Error in updatng")
+            console.log("Error in updating")
           }
           else{
             console.log("Successfully Updated")
@@ -149,6 +166,86 @@ app.delete("/api/questionsdelete", async (req, res) => {
             res.json({ message: "Successfully Deleted"})
           }
         })
+      }
+      else{
+        res.json({ message: "Unauthorized User. Access denied." });
+      }
+    }
+  })
+})
+
+// ADD RESPONSES //
+
+app.post("/api/response", async (req, res) => {
+  await users.findById(req.body.id, async function (err, user) {
+    if(err){
+      console.log(err);
+      console.log("User Not Found")
+      res.json({ message: "User Not Found" });
+    }
+    else{
+      if(user.responses.length === 0){
+        await users.findByIdAndUpdate(req.body.id, { responses: req.body.responses }, (err, docs) => {
+          if(err){
+          console.log(err);
+          console.log("Error in updating the form")
+          }
+          else{
+            console.log("Form Successfully Filled")
+            res.json({ message: "Form Successfully Filled"})
+          }
+        })
+      }
+      else{
+        console.log("Form already filled")
+        res.json({ message: "Form already filled" });
+      }
+    }
+  })
+})
+
+// VIEW INDIVIDUAL RESPONSES //
+
+app.get("/api/individual", async (req, res) => {
+  await users.findById(req.body.id, async function (err, user) {
+    if(err){
+      console.log(err);
+      console.log("User Not Found")
+      res.json({ message: "User Not Found" });
+    }
+    else{
+      if(user.isadmin == true){
+        await users.findById(req.body.uid, async function (error, participant) {
+          if(error){
+            console.log(error);
+            console.log("Participant not found")
+            res.json({ message: "Participant not found" })
+          }
+          else{
+            res.json({ responses: participant.responses})
+          }
+        })
+      }
+      else{
+        res.json({ message: "Unauthorized User. Access denied." });
+      }
+    }
+  })
+})
+
+// VIEW LIST OF PARTICIPANTS //
+
+app.get("/api/participants", async (req, res) => {
+  await users.findById(req.body.id, async function (err, user) {
+    if(err){
+      console.log(err);
+      console.log("User Not Found")
+      res.json({ message: "User Not Found" });
+    }
+    else{
+      if(user.isadmin == true){
+        const uList = await users.find().select('name');
+        res.json({ uList: uList});
       }
       else{
         res.json({ message: "Unauthorized User. Access denied." });
