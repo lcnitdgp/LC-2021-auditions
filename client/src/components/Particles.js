dc9ea855d70fd192fed81594ddb8ca884750e1fd
murtaza1112 <54723469+murtaza1112@users.noreply.github.com>
2021-02-17 03:29:30 +0530

    changes particles

--- client/src/components/Particles.js
+++ client/src/components/Particles.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Particles.css";
 
 function Particles(props) {
-  const numberOfParticles = 115;
+  const numberOfParticles = 50;
   return (
     <>
       {[...Array(numberOfParticles)].map((_, i) => (
