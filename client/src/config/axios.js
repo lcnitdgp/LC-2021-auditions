918b44a5f90f7d172b559ac2832c0996dffd782f
Murtaza <murtaza1112@hotmail.com>
2022-01-10 03:00:30 +0530

    pushed build file to github

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 // Set config defaults when creating the instance
+// pass cookies with every request
+// axios.defaults.withCredentials = true;
+
 const instance = axios.create({
-  baseURL: "/",
+  baseURL: backendUrl,
+  withCredentials: true,
 });
+
+// use the default backend url
 
 export default instance;
