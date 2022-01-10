29951c2bc224829479658f6162e92bf335cff021
Murtaza <murtaza1112@hotmail.com>
2022-01-10 16:39:46 +0530

    changed file structure

--- server/index.js
+++ server/index.js
@@ -55,23 +55,18 @@ app.use(passport.session());
 require("./routes/users")(app);
 require("./routes/questions")(app);
 
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up the index.html file in production
+  //if it dosent recognize the route even on client side
 
-//only in prod version
-//if route for a particular route not recognized on server side
-//then check in client side
+  const path = require("path");
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-//Express will serve up the index.html file
-//if it dosent recognize the route even on client side
-const path = require("path");
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("*", (req, res) => {
-  console.log("Sending front end assets");
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
-
-// }
+  app.get("*", (req, res) => {
+    console.log("Sending front end assets");
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("The server is active on :", PORT);
